5ea502f564fedc4478a341d1c5480f3108912ab7
planit <planit@tanjeffreyz.github.io>
2024-01-23 17:43:19 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,24 +1,50 @@
 assignments = {
-  "EECS 127/227AT-LEC-001": [
+  "CS 61A": [
     {
-      "title": "Homework 01 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-19T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
+      "title": "Lab 0",
+      "course": "CS 61A",
+      "dueDate": "2024-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/701300/assignments/3972330/submissions/223862468",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 61A",
+      "dueDate": "2024-01-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/701300/assignments/3972445/submissions/223906061",
+      "submitted": true
+    },
+    {
+      "title": "Lab 1",
+      "course": "CS 61A",
+      "dueDate": "2024-01-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/701300",
+      "submitted": false
+    }
+  ],
+  "ECON 2": [],
+  "Math 53 Section 210": [
+    {
+      "title": "Homework 1",
+      "course": "Math 53 Section 210",
+      "dueDate": "2024-01-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/716649",
       "submitted": false
     },
     {
-      "title": "Homework 01 Self Grade",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270/assignments/3973130/submissions/new",
+      "title": "Homework 2",
+      "course": "Math 53 Section 210",
+      "dueDate": "2024-02-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/716649",
       "submitted": false
-    },
+    }
+  ],
+  "Physics 5A": [
     {
-      "title": "Homework 02 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
+      "title": "Homework 1",
+      "course": "Physics 5A",
+      "dueDate": "2024-01-26T17:00:00",
+      "link": "https://www.gradescope.com/courses/715569",
       "submitted": false
     }
   ]
