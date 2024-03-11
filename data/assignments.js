843c5431db0528d07caaf77ef6d7bb7e601a7fa2
planit <planit@tanjeffreyz.github.io>
2024-03-11 22:35:57 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -277,8 +277,8 @@ assignments = {
       "title": "Homework 7",
       "course": "Physics 5A",
       "dueDate": "2024-03-11T17:00:00",
-      "link": "https://www.gradescope.com/courses/715569",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/715569/assignments/4209272/submissions/238314663",
+      "submitted": true
     }
   ]
 };
