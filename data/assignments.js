c8120fe11743cf3e88d6e765dcd454f7c79a311f
planit <planit@tanjeffreyz.github.io>
2024-04-22 06:36:56 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -216,6 +216,13 @@ assignments = {
       "dueDate": "2024-04-17T23:59:00",
       "link": "https://www.gradescope.com/courses/701300/assignments/4364562/submissions/247309769",
       "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "CS 61A",
+      "dueDate": "2024-04-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/701300",
+      "submitted": false
     }
   ],
   "ECON 2": [
