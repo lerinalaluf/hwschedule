44226bf138054f86a630beb6c983f7acff010b57
planit <planit@tanjeffreyz.github.io>
2024-05-13 07:35:25 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,13 +1,6 @@
 assignments = {
   "CS 61A": [
     {
-      "title": "Discussion Attendance",
-      "course": "CS 61A",
-      "dueDate": "2024-05-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/701300",
-      "submitted": false
-    },
-    {
       "title": "Lab 0",
       "course": "CS 61A",
       "dueDate": "2024-01-24T23:59:00",
