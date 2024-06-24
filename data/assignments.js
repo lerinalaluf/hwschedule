226dac97951ebe68988c945681787858e32fe0f0
planit <planit@tanjeffreyz.github.io>
2024-06-24 22:36:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,8 +1,8 @@
 assignments = {
-  "Math W53 Sumer 2024": [
+  "Math W53 Summer 2024": [
     {
       "title": "Practice Exam",
-      "course": "Math W53 Sumer 2024",
+      "course": "Math W53 Summer 2024",
       "dueDate": "2024-08-09T15:25:00",
       "link": "https://www.gradescope.com/courses/743617/assignments/4598919/submissions/new",
       "submitted": false
