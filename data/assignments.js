2319dbfc55290a707f54452ec793abf646aa249c
planit <planit@tanjeffreyz.github.io>
2024-06-29 13:36:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,11 +1,1 @@
-assignments = {
-  "Math W53 Summer 2024": [
-    {
-      "title": "Practice Exam",
-      "course": "Math W53 Summer 2024",
-      "dueDate": "2024-08-09T15:25:00",
-      "link": "https://www.gradescope.com/courses/743617/assignments/4598919/submissions/new",
-      "submitted": false
-    }
-  ]
-};
+assignments = {};
