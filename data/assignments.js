82b27f06412060bdbe08fa930d38eb2433ab4708
planit <planit@tanjeffreyz.github.io>
2024-07-17 16:40:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "Quiz for the End of Module 2",
       "course": "Math W53 Summer 2024",
       "dueDate": "2024-07-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/743617/assignments/4606833/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/743617/assignments/4606833/submissions/259629665",
+      "submitted": true
     },
     {
       "title": "Another practice",
