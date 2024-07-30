ad44bfbf3969e23abe9d92c4b5b7a6f27d5fe26a
planit <planit@tanjeffreyz.github.io>
2024-07-30 05:36:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,32 @@
-assignments = {};
+assignments = {
+  "Math W53 Summer 2024": [
+    {
+      "title": "Quiz for the End of Module 1",
+      "course": "Math W53 Summer 2024",
+      "dueDate": "2024-07-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/743617/assignments/4155399/submissions/258623228",
+      "submitted": true
+    },
+    {
+      "title": "Practice Exam",
+      "course": "Math W53 Summer 2024",
+      "dueDate": "2024-08-09T15:25:00",
+      "link": "https://www.gradescope.com/courses/743617/assignments/4598919/submissions/258622920",
+      "submitted": true
+    },
+    {
+      "title": "Quiz for the End of Module 2",
+      "course": "Math W53 Summer 2024",
+      "dueDate": "2024-07-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/743617/assignments/4606833/submissions/259629665",
+      "submitted": true
+    },
+    {
+      "title": "Another practice",
+      "course": "Math W53 Summer 2024",
+      "dueDate": "2024-08-09T20:05:00",
+      "link": "https://www.gradescope.com/courses/743617/assignments/4624544/submissions/new",
+      "submitted": false
+    }
+  ]
+};
