e3cf4e47e8a34042ff406e2d1c80bdf59632c65d
planit <planit@tanjeffreyz.github.io>
2024-08-28 04:39:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,26 @@
 assignments = {
-  "CS 61B": [],
+  "CS 61B": [
+    {
+      "title": "Homework 0A",
+      "course": "CS 61B",
+      "dueDate": "2024-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Homework 0B",
+      "course": "CS 61B",
+      "dueDate": "2024-09-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61B",
+      "dueDate": "2024-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    }
+  ],
   "CS 70": []
 };
