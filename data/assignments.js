be1db4aadfac51f4862cd477ab6556b21df545d6
planit <planit@tanjeffreyz.github.io>
2024-09-23 05:39:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -67,7 +67,7 @@ assignments = {
       "title": "Project 1B: ArrayDeque",
       "course": "CS 61B",
       "dueDate": "2024-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/845096/assignments/4918134/submissions/270313146",
+      "link": "https://www.gradescope.com/courses/845096/assignments/4918134/submissions/270327440",
       "submitted": true
     },
     {
