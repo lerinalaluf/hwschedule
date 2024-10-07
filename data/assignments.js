f424f4078d9c4a959f83a7192df001aef46c65ed
planit <planit@tanjeffreyz.github.io>
2024-10-07 10:42:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -232,6 +232,13 @@ assignments = {
       "dueDate": "2024-10-05T16:00:00",
       "link": "https://www.gradescope.com/courses/836005",
       "submitted": false
+    },
+    {
+      "title": "Homework 6",
+      "course": "CS 70",
+      "dueDate": "2024-10-12T16:00:00",
+      "link": "https://www.gradescope.com/courses/836005",
+      "submitted": false
     }
   ],
   "Physics 5B": [
