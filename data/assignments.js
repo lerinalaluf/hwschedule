cfeb672828b6f5f8f347664341d29eb909075ba6
planit <planit@tanjeffreyz.github.io>
2024-10-20 16:41:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -516,6 +516,13 @@ assignments = {
       "dueDate": "2024-10-15T10:10:00",
       "link": "https://www.gradescope.com/courses/849578",
       "submitted": false
+    },
+    {
+      "title": "Pre-Lab T1.EM3 (RC and RLC circuits)",
+      "course": "Physics 5BL",
+      "dueDate": "2024-10-22T10:10:00",
+      "link": "https://www.gradescope.com/courses/849578",
+      "submitted": false
     }
   ]
 };
