c978b24e8cb0ce582fcce5e0449ef7c99c43f8e9
planit <planit@tanjeffreyz.github.io>
2024-10-30 03:03:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -335,7 +335,7 @@ assignments = {
       "title": "Mini-Vitamin 9A",
       "course": "CS 70",
       "dueDate": "2024-10-29T19:30:00",
-      "link": "https://www.gradescope.com/courses/836005/assignments/5208804/submissions/new",
+      "link": "https://www.gradescope.com/courses/836005",
       "submitted": false
     },
     {
