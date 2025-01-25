56128fd94732b291b067199bd084b8d450172c33
planit <planit@tanjeffreyz.github.io>
2025-01-25 07:35:53 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "PHYS 89": [],
+  "PHYS 89": [
+    {
+      "title": "89 HW 1",
+      "course": "PHYS 89",
+      "dueDate": "2025-02-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/959672",
+      "submitted": false
+    }
+  ],
   "PHYSICS 5C-LEC-001": []
 };
