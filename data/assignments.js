7032d6678bfa8b6e07395241979cd05a1ae40077
planit <planit@tanjeffreyz.github.io>
2025-02-17 23:35:50 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -80,6 +80,13 @@ assignments = {
       "dueDate": "2025-02-11T10:00:00",
       "link": "https://www.gradescope.com/courses/970304",
       "submitted": false
+    },
+    {
+      "title": "Pre-Lab 2",
+      "course": "Physics 5CL - 101",
+      "dueDate": "2025-02-25T10:00:00",
+      "link": "https://www.gradescope.com/courses/970304",
+      "submitted": false
     }
   ]
 };
