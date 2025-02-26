f7bcb46757fb48e7cfece6e35f46e2bb25d68ec8
planit <planit@tanjeffreyz.github.io>
2025-02-26 21:35:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -84,7 +84,7 @@ assignments = {
     {
       "title": "Lab Report 1",
       "course": "Physics 5CL - 101",
-      "dueDate": "2025-02-28T19:00:00",
+      "dueDate": "2025-03-03T23:59:00",
       "link": "https://www.gradescope.com/courses/970304",
       "submitted": false
     },
