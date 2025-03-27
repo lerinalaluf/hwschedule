649f8b3bb4182cd1c11bafe7059fe58734bb8156
planit <planit@tanjeffreyz.github.io>
2025-03-27 17:37:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -113,6 +113,13 @@ assignments = {
       "dueDate": "2025-03-21T23:59:00",
       "link": "https://www.gradescope.com/courses/957311",
       "submitted": false
+    },
+    {
+      "title": "HW09",
+      "course": "PHYSICS 5C-LEC-001",
+      "dueDate": "2025-04-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/957311",
+      "submitted": false
     }
   ],
   "Physics 5CL - 101": [
